e6a62c42179db3c8f3b5948c106f609ab9414c8c
bhagyashree-27 <66769985+bhagyashree-27@users.noreply.github.com>
2020-07-03 10:28:27 +0530

    change

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("New");
 		System.out.println("Hello");
-		System.out.println("Test");
+		//System.out.println("Test");
 	}
 
 }
